447af1317e8c7dfdb1e31bb519b9ede6e8b63841
v1rtl <hi@v1rtl.site>
2025-04-05 20:29:39 +0300

    fix: dnslink crashing on `blumen deploy`

--- src/actions/deploy.ts
+++ src/actions/deploy.ts
@@ -116,6 +116,6 @@ export const deployAction = async (
   }
 
   if (dnslink) {
-    await dnsLinkAction({ cid, name, options: { verbose } })
+    await dnsLinkAction({ cid, name: dnslink, options: { verbose } })
   }
 }
