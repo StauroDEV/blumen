1089c09f57ae850717d3e6fd2026ce7dd2be73cb
v1rtl <hi@v1rtl.site>
2025-04-17 13:12:16 +0300

    fix codec

--- src/utils/swarm.ts
+++ src/utils/swarm.ts
@@ -5,10 +5,10 @@ import { hexToBytes } from 'viem/utils'
 import { create as createMultihashDigest } from 'multiformats/hashes/digest'
 
 const KECCAK_256_CODEC = 0x1b
-const SWARM_FEED_CODEC = 0xfb
+const SWARM_MANIFEST_CODEC = 0xfa
 
 export const referenceToCID = (ref: Hex) => {
   const hashBytes = hexToBytes(ref)
 
-  return CID.createV1(SWARM_FEED_CODEC, createMultihashDigest(KECCAK_256_CODEC, hashBytes)).toString()
+  return CID.createV1(SWARM_MANIFEST_CODEC, createMultihashDigest(KECCAK_256_CODEC, hashBytes)).toString()
 }
