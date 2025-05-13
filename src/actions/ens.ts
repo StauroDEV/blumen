96c6422bbf94e00015af3001542d9eb910576e40
v1rtl <hi@v1rtl.site>
2025-05-14 01:56:56 +0300

    chore: biome

--- src/actions/ens.ts
+++ src/actions/ens.ts
@@ -181,7 +181,8 @@ export const ensAction = async ({
         })
         const safeLink = `https://app.safe.global/transactions/queue?safe=${safeAddress}`
         logger.success(
-          `Transaction proposed to a Safe wallet.\nOpen in a browser: ${isTTY ? colors.underline(safeLink) : safeLink
+          `Transaction proposed to a Safe wallet.\nOpen in a browser: ${
+            isTTY ? colors.underline(safeLink) : safeLink
           }`,
         )
       } catch (e) {
