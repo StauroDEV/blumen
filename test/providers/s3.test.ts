dc4fa36adeb10fe8929d23a2bd06617fc0f1a23c
v1rtl <hi@v1rtl.site>
2025-05-14 14:17:08 +0300

    chore: lint

--- test/providers/s3.test.ts
+++ test/providers/s3.test.ts
@@ -1,38 +1,44 @@
-import { describe, mock, it, expect } from 'bun:test'
-import { uploadOnS3 } from '../../src/providers/ipfs/s3'
+import { describe, expect, it, mock } from 'bun:test'
 import { DeployError } from '../../src/errors'
-
+import { uploadOnS3 } from '../../src/providers/ipfs/s3'
 
 const success = new Response(null, { status: 200 })
 const failure = new Response('mock', { status: 400 })
 
-
 describe('s3', () => {
-
   it('should upload a car to s3', async () => {
     mock.module('@stauro/filebase-upload', async () => {
       return {
-        uploadCar: async () => success
+        uploadCar: async () => success,
       }
     })
     const res = await uploadOnS3({
-      name: 'test.car', car: new Blob([]),
-      token: 'token', bucketName: 'bucketName',
-      apiUrl: 'https://example.com', providerName: 'S3', verbose: false
+      name: 'test.car',
+      car: new Blob([]),
+      token: 'token',
+      bucketName: 'bucketName',
+      apiUrl: 'https://example.com',
+      providerName: 'S3',
+      verbose: false,
     })
     expect(res.status).toEqual(success.status)
   })
   it('should throw if upload fails', async () => {
     mock.module('@stauro/filebase-upload', async () => {
       return {
-        uploadCar: async () => failure
+        uploadCar: async () => failure,
       }
     })
-    expect(uploadOnS3({
-      name: 'test.car', car: new Blob([]),
-      token: 'token', bucketName: 'bucketName',
-      apiUrl: 'https://example.com', providerName: 'S3', verbose: false
-    })
+    expect(
+      uploadOnS3({
+        name: 'test.car',
+        car: new Blob([]),
+        token: 'token',
+        bucketName: 'bucketName',
+        apiUrl: 'https://example.com',
+        providerName: 'S3',
+        verbose: false,
+      }),
     ).rejects.toThrow(DeployError)
   })
 })
