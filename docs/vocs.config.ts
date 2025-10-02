b3f87ca009a134621594d3a5740a56ef9af2f6ba
v1rtl <hi@v1rtl.site>
2025-10-03 01:44:25 +0300

    docs: add "Used by" with sick logos

--- docs/vocs.config.ts
+++ docs/vocs.config.ts
@@ -3,91 +3,134 @@ import { defineConfig } from 'vocs'
 
 export default defineConfig({
   title: 'Blumen',
-  socials: [
-    { icon: 'github', link: 'https://github.com/StauroDEV/blumen' },
-  ],
-
-  sidebar: [
-      {
-        text: 'Introduction',
-        link: '/',
-        items: [
+  description: 'Deploy apps on the decentralized web without compromises',
+  socials: [{ icon: 'github', link: 'https://github.com/StauroDEV/blumen' }],
+  sponsors: [
+    // {
+    //   name: 'Collaborator',
+    //   height: 80,
+    //   items: [
+    //     [
+    //       {
+    //         name: 'Lido',
+    //         link: 'https://lido.fi',
+    //         image: '/lido.svg',
+    //       },
+    //     ],
+    //   ],
+    // },
+    {
+      name: 'Used by',
+      height: 80,
+      items: [
+                [
           {
-            text: 'How it works',
-            link: '/docs/how-it-works',
-          },
-          {
-            text: 'Installation',
-            link: '/docs/install',
-          },
-          {
-            text: 'Getting Started',
-            link: '/docs/get-started',
+            name: 'Lido',
+            link: 'https://lido.fi',
+            image: '/lido.svg',
           },
         ],
-      },
-      {
-        text: 'Guides',
-        items: [
+        [
           {
-            text: 'IPFS',
-            link: '/docs/ipfs',
+            name: 'WalletBeat',
+            link: 'https://beta.walletbeat.eth.limo',
+            image: '/WalletBeat.svg',
           },
           {
-            text: 'Swarm',
-            link: '/docs/swarm',
+            name: 'StorageBeat',
+            link: 'https://storagebeat.eth.link',
+            image: '/StorageBeat.svg',
           },
+        ],
+        [
           {
-            text: 'Deploying with Safe',
-            link: '/docs/safe',
-          },
-          {
-            text: 'DNSLink',
-            link: '/docs/dnslink',
-          },
-          {
-            text: 'CI/CD',
-            link: '/docs/ci-cd',
-          },
+            name: 'v1rtl.site',
+            link: 'https://v1rtl.eth.link',
+            image: '/v1rtl-site.webp',
+          }
         ]
-      },
-      {
-        text: 'CLI Reference',
-        items: [
-          {
-            text: 'Deploy',
-            link: '/cli/deploy',
-          },
-          {
-            text: 'Status',
-            link: '/cli/status',
-          },
-          {
-            text: 'ENS',
-            link: '/cli/ens',
-          },
-          {
-            text: 'Ping',
-            link: '/cli/ping',
-          },
-          {
-            text: 'DNSLink',
-            link: '/cli/dnslink',
-          },
-          {
-            text: 'Pack',
-            link: '/cli/pack',
-          },
-          {
-            text: 'Pin',
-            link: '/cli/pin',
-          },
-        ],
-      },
-    ],
+      ],
+    },
+  ],
+  sidebar: [
+    {
+      text: 'Introduction',
+      link: '/',
+      items: [
+        {
+          text: 'How it works',
+          link: '/docs/how-it-works',
+        },
+        {
+          text: 'Installation',
+          link: '/docs/install',
+        },
+        {
+          text: 'Getting Started',
+          link: '/docs/get-started',
+        },
+      ],
+    },
+    {
+      text: 'Guides',
+      items: [
+        {
+          text: 'IPFS',
+          link: '/docs/ipfs',
+        },
+        {
+          text: 'Swarm',
+          link: '/docs/swarm',
+        },
+        {
+          text: 'Deploying with Safe',
+          link: '/docs/safe',
+        },
+        {
+          text: 'DNSLink',
+          link: '/docs/dnslink',
+        },
+        {
+          text: 'CI/CD',
+          link: '/docs/ci-cd',
+        },
+      ],
+    },
+    {
+      text: 'CLI Reference',
+      items: [
+        {
+          text: 'Deploy',
+          link: '/cli/deploy',
+        },
+        {
+          text: 'Status',
+          link: '/cli/status',
+        },
+        {
+          text: 'ENS',
+          link: '/cli/ens',
+        },
+        {
+          text: 'Ping',
+          link: '/cli/ping',
+        },
+        {
+          text: 'DNSLink',
+          link: '/cli/dnslink',
+        },
+        {
+          text: 'Pack',
+          link: '/cli/pack',
+        },
+        {
+          text: 'Pin',
+          link: '/cli/pin',
+        },
+      ],
+    },
+  ],
   markdown: {
-    rehypePlugins: [
-      rehypeMermaid,
-    ]
-  }
+    rehypePlugins: [rehypeMermaid],
+  },
 })
